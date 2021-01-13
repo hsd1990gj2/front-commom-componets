413c9ee0cd4d08c9580e3213688524bf1bd8d428
chaizhixiang <chaizhixiang@nequal.com>
2021-01-13 17:00:44 +0800

    chore:第三方依赖lodash

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import _ from './node_modules/lodash-es/lodash.default.js'
+
 export default {
     add(a, b){
         return a+b;
+    },
+    test(){
+        let arr = [1,2]
+        let newArr = _.map(arr, String)
+        console.log(newArr);
     }
 }
