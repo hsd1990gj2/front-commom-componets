4e298a9585caa4f59cc4b14fb10857bb899a4ffb
chaizhixiang <chaizhixiang@nequal.com>
2021-01-21 15:25:20 +0800

    test:npm

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import _ from './node_modules/lodash-es/lodash.default.js'
+// import _ from './node_modules/lodash-es/lodash.default.js'
 
 export default {
     add(a, b){
         return a+b;
-    },
-    test(){
-        let arr = [1,2]
-        let newArr = _.map(arr, String)
-        console.log(newArr);
     }
+    // test(){
+    //     let arr = [1,2]
+    //     let newArr = _.map(arr, String)
+    //     console.log(newArr);
+    // }
 }
